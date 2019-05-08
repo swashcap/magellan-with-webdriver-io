31c8a8a36065a99d488468162915b39f39c0d941
Cory Reed <swashcap@gmail.com>
2019-05-07 23:33:10 -0700

    feat: add static file serving

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import 'hard-rejection/register'
 
-import hapi from '@hapi/hapi'
 import good from '@hapi/good'
+import hapi from '@hapi/hapi'
+import inert from '@hapi/inert'
+import path from 'path'
 
 const port = 4000
 
 const getServer = async () => {
   const server = new hapi.Server({ port })
+
+  await server.register(inert)
 
   if (process.env.NODE_ENV !== 'test') {
     await server.register({
@@ -31,6 +35,18 @@ const getServer = async () => {
       plugin: good
     })
   }
+
+  server.route({
+    handler: {
+      directory: {
+        index: false,
+        path: path.resolve(__dirname, '../assets'),
+        redirectToSlash: true
+      }
+    },
+    method: 'GET',
+    path: '/assets/{param*}'
+  })
 
   return server
 }
