95b49583cfa1766918416cf3940c31273113282a
Cory Reed <swashcap@gmail.com>
2019-05-09 10:47:09 -0700

    feat: add login form

--- src/client.tsx
+++ src/client.tsx
@@ -5,6 +5,17 @@ const App = () => (
   <div>
     <h1>Hello, World!</h1>
     <p>This is a sample React application.</p>
+    <form action="/login" method="POST">
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input id="username" name="username" />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" />
+      </div>
+      <button type="submit">Log In</button>
+    </form>
   </div>
 )
 
