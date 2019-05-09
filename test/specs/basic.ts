389084476ddeb364d3b7c8d8d6cf027b8362fbb1
Cory Reed <swashcap@gmail.com>
2019-05-08 23:17:01 -0700

    test: correct title assertion

--- test/specs/basic.ts
+++ test/specs/basic.ts
@@ -4,6 +4,7 @@ import { assert } from 'chai'
 describe('home page', () => {
   it('should have the right title', async () => {
     browser.url('http://localhost:4000')
-    assert.equal(await browser.getTitle(), 'hello')
+    const title = await browser.getTitle()
+    assert.equal(title, 'Magellan with WebdriverIO')
   })
 })
