c8ffbef915a1f4d211fe86189a298ad2ee0e522f
Cory Reed <swashcap@gmail.com>
2019-05-10 09:57:24 -0700

    refactor: move test scripts to npm-scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,11 +12,9 @@ kill $(lsof -ti :4000 && lsof -ti :4444) &> /dev/null || true
 
 # Assumes a freshly built server
 yarn -s start > logs/server.log 2>&1 &
-node_modules/.bin/geckodriver --log debug > logs/geckodriver.log 2>&1 &
+yarn -s test:geckodriver --log debug > logs/geckodriver.log 2>&1 &
 
-node_modules/.bin/wait-on --log http-get://localhost:4000 \
-  http-get://localhost:4444/status && \
-  node_modules/.bin/wdio -c wdio.conf.js
+yarn -s test:wdio
 
 WDIO_EXIT_CODE=$?
 
